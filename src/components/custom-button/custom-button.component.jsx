8cd34e8db66cf7db22f9b47dce8b708f7a50728e
Pimpuk Sansuth <pimpuks@yahoo.com>
2019-10-23 22:19:25 +0800

    Update code to use styled component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+// import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
 const CustomButton = ({
+  // These props are for non-styled components
+  // children,
+  // isGoogleSignIn,
+  // inverted,
+  // ...otherProps
   children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
+  ...props
 }) => {
   return (
-    <button
-      className={`${inverted ? 'inverted-sign-in' : ''} ${
-        isGoogleSignIn ? 'google-sign-in' : ''
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
+    <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+    // Original code used with scss file (i.e. non-styled component)
+    // <button
+    //   className={`${inverted ? 'inverted-sign-in' : ''} ${
+    //     isGoogleSignIn ? 'google-sign-in' : ''
+    //   } custom-button`}
+    //   {...otherProps}
+    // >
+    //   {children}
+    // </button>
   );
 };
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils';
@@ -9,33 +9,33 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from './header.styles';
+// import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className='logo'></Logo>
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>SHOP</OptionLink>
+      <OptionLink to='/shop'>CONTACT</OptionLink>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <OptionLink as='div' onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionLink>
       ) : (
-        <Link to='/signin' className='option'>
-          SIGN IN{' '}
-        </Link>
+        <OptionLink to='/signin'>SIGN IN </OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 // Without selector
 // const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
